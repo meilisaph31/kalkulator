f29bab0d4c05340957af25c5dc6e13db674eead0
Meilisa PH <88617915+meilisaph31@users.noreply.github.com>
2021-11-23 17:08:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,75 @@
 const calculatorScreen = document.querySelector('.calculator-screen')
-
-const updateScreen = (number) => {
-    calculatorScreen.value = number
-}
-
-const numbers = document.querySelectorAll(".number")
-
-numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        inputNumber(event.target.value)
-        updateScreen(currentNumber)
-    })
-})
+const numbers=document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const equalSign = document.querySelector('.equal-sign')
+const clearBtn = document.querySelector('.all-clear')
+const decimal = document.querySelector('.decimal')
 
 let prevNumber = ''
 let calculationOperator = ''
 let currentNumber = '0'
 
-const inputNumber = (number) => {
-    if (currentNumber === '0') {
+const updateScreen = (number)=>{
+    calculatorScreen.value = number
+}
+const inputNumber = (number)=>{
+    if(currentNumber==='0'){
         currentNumber = number
-    } else {
+    } else{
         currentNumber += number
     }
 }
-
-const operators = document.querySelectorAll(".operator")
-
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value)
-    })
-})
-
-const inputOperator = (operator) => {
-    if (calculationOperator === '') {
+const inputOperator = (operator)=>{
+    if(calculationOperator===''){
         prevNumber = currentNumber
     }
     calculationOperator = operator
     currentNumber = '0'
+
 }
 
-const equalSign = document.querySelector('equal-sign')
-
-equalSign.addEventListener('click', () => {
-    calculate()
-    updateScreen(currentNumber)
-})
-
-
-const clearBtn = document.querySelector('.all-clear')
-
-clearBtn.addEventListener('click', () => {
-    clearAll()
-    updateScreen(currentNumber)
-})
-
-const clearAll = () => {
+const clearAll=()=>{
     prevNumber = ''
     calculationOperator = ''
     currentNumber = '0'
 }
+inputDecimal = (dot)=>{
+    if(currentNumber.includes('.')){
+        return
+    }
+    currentNumber += dot
+}
 
-const decimal = document.querySelector('decimal')
+numbers.forEach((number)=>{
+    number.addEventListener("click",(event)=>{
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
 
-decimal.addEventListener('click', (event) => {
+operators.forEach((operator)=>{
+    operator.addEventListener("click",(event)=>{
+        inputOperator(event.target.value)
+    })
+})
+
+equalSign.addEventListener('click',()=>{
+    calculate()
+    updateScreen(currentNumber)
+})
+
+clearBtn.addEventListener('click',()=>{
+    clearAll()
+    updateScreen(currentNumber)
+})
+decimal.addEventListener('click',(event)=>{
     inputDecimal(event.target.value)
     updateScreen(currentNumber)
 })
 
-inputDecimal = (dot) => {
-    if(currentNumber.includes(',')) {
-        return}
-    currentNumber += dot
-}
-
-const calculate = () => {
+const calculate = () =>{
     let result = ''
-    switch(calculationOperator) {
+    switch(calculationOperator){
         case "+":
             result = parseFloat(prevNumber) + parseFloat(currentNumber)
             break
@@ -88,20 +80,20 @@ const calculate = () => {
             result = parseFloat(prevNumber) * parseFloat(currentNumber)
             break
         case "/":
-            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            result = parseFloat(prevNumber)/parseFloat(currentNumber)
             break
         case "%":
-            result = parseFloat(prevNumber) / parseFloat(100)
+            result = parseFloat(prevNumber)/parseFloat(100)
             break
         case "^":
             result = Math.pow(prevNumber,currentNumber)
             break
         case "sqrt":
-            result = Math.sqrt(prevNumber)
+            result=Math.sqrt(prevNumber)
             break
-        default:
+        default :
             return
     }
-    currentNumber = result
+    currentNumber= result
     calculationOperator = ''
 }
